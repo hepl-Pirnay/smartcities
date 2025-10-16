40a8f62e2eff208bf8785412a43a9c491410a060
hepl-Pirnay <florent.pirnay@student.hepl.be>
2025-10-16 16:31:39 +0200

    Fichier lecture du capteur hum et temp
    programme qui permet de lire la valeur d'un capteur d'humidite et le transcrire sur le lcd

---
+++ LABO_3/DHT_HUMIDITY_SENSOR.py
@@ -0,0 +1,21 @@
+from LCD1602 import LCD1602
+from dht import DHT11
+from machine import I2C,Pin,ADC
+from utime import sleep
+
+i2c = I2C(1,scl=Pin(7), sda=Pin(6), freq=400000)
+d = LCD1602(i2c, 2, 16)
+d.display()
+sensor = DHT11(Pin(18))
+
+while True:
+    sensor.measure()
+    temp=sensor.temperature()
+    humid=sensor.humidity()
+    d.clear()
+    d.setCursor(0,0)
+    d.print("Temp:"+str(temp))
+    d.setCursor(0,1)
+    d.print("Humid:"+str(humid))
+    sleep(1)
+    

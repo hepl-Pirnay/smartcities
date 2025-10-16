f3e93ece852e4af6604f9fcc868027d6831b0959
hepl-Pirnay <florent.pirnay@student.hepl.be>
2025-10-16 17:33:00 +0200

    Programme de l'exercice 3
    le programme est en travaille 
    verifier pq il ne rentre pas dans l'etat 2 mais prend bien l'etat un peut etre voir pour faire une priorite de l'un a l'autre

---
+++ LABO_3/LABO_3_PIRNAY.py
@@ -0,0 +1,61 @@
+'''Programme de l'exercice 3'''
+
+#---Librairie setup-----
+from LCD1602 import LCD1602 #importation de la fonction LCD1602 depuis la librairie LCD1602  
+from dht import DHT11  		# importation de la fonction DHT11 depuis la librairie dht
+from machine import I2C,Pin,ADC,PWM # importation des fonctions pin,adc,pwm,i2c depuis la librairie machine
+from utime import sleep  #importation de la fonction sleep depuis la librairie time
+
+
+#---Hardware setup----
+i2c = I2C(1,scl=Pin(7), sda=Pin(6), freq=400000) # Initialise le bus I²C numéro 1.
+                                                 # scl (clock) sur la broche GPIO7 et sda (data) sur GPIO6. Vitesse du bus : 400 kHz.
+d = LCD1602(i2c, 2, 16) 						 #Crée un objet d pour piloter un écran LCD 16 colonnes × 2 lignes via I²C.
+d.display() 									 # active l’affichage (allume l’écran).
+sensor = DHT11(Pin(18)) 						 #Initialise le capteur DHT11 connecté sur la broche GPIO18.
+buzzer = PWM(Pin(16))   						 #Crée un signal PWM (modulation de largeur d’impulsion) sur la broche 16.
+pot = ADC(0)
+led = Pin(20,Pin.OUT)
+
+
+#-----Main loop-----
+while True:
+    #----Mapping du pot----
+    val = pot.read_u16()  # lecture brute entre 0 et 65535
+    valeur_consigne = 15 + (val * (35 - 15)/65535)  # mappe entre 15 et 35
+    print("Set", round(valeur_consigne, 1))
+    sleep(0.5)
+    
+    #----Lecture du capteur temperature-----
+    sensor.measure()
+    temp=sensor.temperature()
+    sleep(1)
+    #----Affichage sur le LCD------
+    d.clear()
+    d.setCursor(0,0)
+    d.print("Temp consigne:"+str(valeur_consigne))
+    d.setCursor(0,1)
+    d.print("Temp ambiante:"+str(temp))
+    
+    if temp > valeur_consigne :
+        led.value(1)   # allume la LED
+        sleep(1)       # attend 1 seconde
+        led.value(0)   # éteint la LED
+        sleep(1) 	   # attend 1 seconde
+        print("etat 1")
+        buzzer.freq(1000)
+        buzzer.duty_u16(1000)
+    elif temp+3 > valeur_consigne:
+        print("etat 2")
+        buzzer.freq(1000)
+        buzzer.duty_u16(1000)
+        led.value(1)   # allume la LED
+        sleep(0.25)       # attend 1 seconde
+        led.value(0)   # éteint la LED
+        sleep(0.25)
+        d.clear()
+        d.setCursor(0,0)
+        d.print("ALARM")
+        
+    else:
+        buzzer.duty_u16(0)#close
